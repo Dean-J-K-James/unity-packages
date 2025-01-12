388baad04614a40531695a734694b20d078fdd70
Dean-J-K-James <60291976+Dean-J-K-James@users.noreply.github.com>
2025-01-12 09:31:14 +0000

    Update BiomeManager.cs

--- unity-template-biome/biome/Biome/BiomeManager.cs
+++ unity-template-biome/biome/Biome/BiomeManager.cs
@@ -9,10 +9,15 @@ using UnityEngine;
  */
 public class BiomeManager : MonoBehaviour
 {
-    public string biome; //
-
-    /**
+	/**
      * 
      */
-    void Start() { var b = Instantiate(Asset.INSTANCE.Get<Biome>(biome), transform); b.gameObject.SetActive(true); b.InvokeCreated(); }
+	void Start()
+	{
+		string biome = PlayerPrefs.GetString("level", "dungeon");
+
+		var b = Instantiate(Asset.INSTANCE.Get<Biome>(biome), transform);
+		b.gameObject.SetActive(true);
+		b.InvokeCreated();
+	}
 }
